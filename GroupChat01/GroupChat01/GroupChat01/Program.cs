9bc8b3ee7d5a1d167acb025badd7406716da7d01
Garrett Girod <girodg@hotmail.com>
2023-01-04 20:26:15 -0500

    group chat 01

---
+++ GroupChat01/GroupChat01/GroupChat01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GroupChat01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Bruce", "Batman", "The Dark Knight", "Bats", "The Bat" };
+
+
+            string name = string.Empty ;
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (names[i].Equals("THE DARK KNIGHT",StringComparison.OrdinalIgnoreCase))
+                {
+                    name = names[i];
+                    break;
+                }
+            }
+            Console.WriteLine($"Found {name}");
+        }
+
+    }
+}
