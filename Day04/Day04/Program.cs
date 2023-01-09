13c67a756ce518744a3d2134c6614c71d1ce7bca
Garrett Girod <girodg@hotmail.com>
2023-01-09 10:24:49 -0500

    bubble sort

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -22,7 +22,7 @@ namespace Day04
               
             */
             int N = 0;
-            RecursiveLoop(N);
+            //RecursiveLoop(N);
             Console.ResetColor();
 
 
@@ -70,6 +70,48 @@ namespace Day04
                     
             */
 
+            string s1 = "Batman", s2 = "Batmen";
+            //s1.CompareTo(s2)
+            //  returns 3 possible values:
+            //  -1  less than
+            //   0  equal
+            //   1  greater than
+            int result = s1.CompareTo(s2);
+            if (result == 0) Console.WriteLine($"{s1} EQUALS {s2}");
+            else if(result == -1) Console.WriteLine($"{s1} LESS THAN {s2}");
+            else if (result == 1) Console.WriteLine($"{s1} GREATER THAN {s2}");
+
+            List<int> numbers = new List<int>() { 5, 4, 3, 2, 1 };
+            List<string> names = new() { "Bruce", "Clark", "Adam", "Dave", "Bob"};
+            BubbleSort(names);
+            foreach(string name in names)
+                Console.WriteLine(name);
+            //numbers.Sort();
+
+        }
+        static void BubbleSort(List<string> A)
+        {
+            int n = A.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i <= n-1; i++)
+                {
+                    if (A[i - 1].CompareTo(A[i]) > 0)
+                    {
+                        //swap logic
+                        //Swap(A[i- 1], A[i]);
+                        //int temp = A[i - 1];
+                        //A[i - 1] = A[i];
+                        //A[i] = temp;
+                        (A[i], A[i-1]) = (A[i-1], A[i]);
+                        swapped = true;
+                    }
+                }
+                //  n = n - 1;
+                --n;
+            } while (swapped);
         }
 
 
