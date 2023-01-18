7463837b23b2651f3c9f0eb40943030488edec83
Garrett Girod <girodg@hotmail.com>
2023-01-18 09:59:53 -0500

    Fields

---
+++ Day07/Day07CL/GameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class GameObject
+    {
+        #region Fields
+        private int _x, _y;
+        #endregion
+
+        void Draw()
+        {
+            Console.SetCursorPosition(_x, _y);
+        }
+    }
+}
